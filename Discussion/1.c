9705125fd2b8e56a27423c4dc6cc4719dc3ab087
Rodney Osodo <blackd0t@protonmail.com>
2019-01-20 13:42:18 +0300

    Adding discussion during meetup

---
+++ Discussion/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+
+{
+	//This is a program to output hello world to the screen
+    printf("Hello world\n");
+    //\n is used to mean next line
+    return 0;
+    
+}
---
+++ Discussion/2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+
+{
+
+	int age = 67;
+	float gpa = 3.897;
+    printf("Name\t:\tOsodo Rodney\n");
+    printf("Age\t:\t%d\n", age);
+    printf("Course\t:\tBlahblahblah\n");
+    printf("GPA\t:\t%f",gpa);
+    return 0;
+
+}
