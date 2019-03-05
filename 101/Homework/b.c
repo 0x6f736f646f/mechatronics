49e9fc0ceb7f09b2619273e9262e33b30772aca7
Rodney Osodo <blackd0t@protonmail.com>
2019-03-05 07:48:49 +0300

    Modifying b.c

--- 101/Homework/b.c
+++ 101/Homework/b.c
@@ -7,9 +7,14 @@ int main ()
 	float var2 = 4.5;
 	double var3 = 3.1428571428571428571428571428571;
 	long var4 = 3142856756;
+	unsigned int var5 = 20;
+	unsigned long var6 = 3142856756;
 	printf("Variable 1: %d\n", var1);
 	printf("Variable 2: %f\n", var2);
-	printf("Variable 3: %g\n", var3);
+	printf("Variable 3: %lf\n", var3);
+	printf("Variable 4: %ld\n", var4);
+	printf("Variable 5: %u\n", var5);
+	printf("Variable 6: %lu\n", var6);
 	//printf("Variable 4: ", var4);
     return 0;
     
